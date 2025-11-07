758449375dcdd9909e778e09338ef3f9d8f309d7
xkippox <159185118+xkippox@users.noreply.github.com>
2025-11-07 08:32:11 -0300

    Atualizar o Prova_1_InternetOfThings.ino
    Termino do trabalho,completo.

--- Prova_1_InternetOfThings.ino
+++ Prova_1_InternetOfThings.ino
@@ -1,112 +1,105 @@
+// Definindo as constantes e seus nomes.
 const int LDR = 1;
 const int LED_AZUL = 12;
-const int LED_VERMELHO = 14;
 const int LED_VERDE = 13;
+const int LED_VERMELHO = 14;
+const int BOTAO_MODO = 2;
+const int BOTAO_CORES = 3;
 
-const int BOTAO_1 = 7;
-const int BOTAO_2 = 6;
 
 void setup() {
 
-  pinMode(LED_AZUL, OUTPUT); //definindo o LED AZUL como uma saída.
+  // Definindo as entradas e saídas.
+  pinMode(LDR, INPUT);
+  pinMode(LED_AZUL, OUTPUT);
   pinMode(LED_VERDE, OUTPUT);
   pinMode(LED_VERMELHO, OUTPUT);
-
-  pinMode(BOTAO_1, INPUT_PULLDOWN);
-  pinMode(BOTAO_2, INPUT_PULLDOWN);
-
-  pinMode(LDR, INPUT);
+  pinMode(BOTAO_MODO, INPUT_PULLUP);
+  pinMode(BOTAO_CORES, INPUT_PULLUP);
   Serial.begin(9600);
 
 }
 
-int leituraLDR; //criei uma váriavel que vai ler o valor do LDR.
-int brilhoLED; // criei uma váriavel que vai converter o bagulho.
-bool manual = true;
+int leituraLDR;
+int ldrConvertido;
+bool manual = false;
+int contador = 1;
 
 void loop() {
 
-  leituraLDR = analogRead(LDR); // essa váriavel pega a leitura analogica do Pino LDR.
-  brilhoLED = map(leituraLDR, 0, 8191, 0, 255); // faz a transcrição da leitura analogica, pro pino.
-  int brilhoOposto = 255 - brilhoLED; // Aqui foi só pra inverter, quanto mais escuro, mais luz, quanto menos, menos luz.
-  int contador = 0;
+  leituraLDR = analogRead(LDR);
+  ldrConvertido = map(leituraLDR, 0, 8191, 0, 255);
+  Serial.println(leituraLDR);
+  Serial.println(ldrConvertido);
 
-  if(digitalRead(BOTAO_1) == LOW) {
-
-    if(digitalRead(BOTAO_1) == LOW) {
-      manual = !manual;
-    }
-
-    if(manual = false){
-
-      if (brilhoOposto > 200) {
-        digitalWrite(LED_AZUL, HIGH);
-        digitalWrite(LED_VERDE, LOW);
-        digitalWrite(LED_VERMELHO, LOW);
-        Serial.println(brilhoOposto);
-        delay(500);
-      }
-
-
-      else if(brilhoOposto < 200 && brilhoOposto > 100) {
-        digitalWrite(LED_VERDE, HIGH);
-        digitalWrite(LED_AZUL, LOW);
-        digitalWrite(LED_VERMELHO, LOW);
-        Serial.println(brilhoOposto);
-        delay(500);
-      }
-
-      else if(brilhoOposto < 100 && brilhoOposto > 1) {
-        digitalWrite(LED_VERMELHO, HIGH);
-        digitalWrite(LED_AZUL, LOW);
-        digitalWrite(LED_VERDE, LOW);
-        Serial.println(brilhoOposto);
-        delay(500);
-      }
-
-      else {
-        digitalWrite(LED_VERMELHO, LOW);
-        digitalWrite(LED_AZUL, LOW);
-        digitalWrite(LED_VERDE, LOW);
-        Serial.println(brilhoOposto);
-      }
-
-      Serial.println(brilhoOposto);
-      delay(500);
-
-    }
-
-
-    else if(manual = true) {
-      if(digitalRead(BOTAO_2) == LOW) {
-        contador = contador + 1;
-
-        if(contador = 1){
-          digitalWrite(LED_VERMELHO, HIGH);
-          digitalWrite(LED_VERDE, LOW);
-          digitalWrite(LED_AZUL, LOW);
-        }
-
-        if(contador = 2){
-          digitalWrite(LED_VERMELHO, HIGH);
-          digitalWrite(LED_VERDE, LOW);
-          digitalWrite(LED_AZUL, LOW);
-        }
-
-        if(contador = 3){
-          digitalWrite(LED_VERMELHO, HIGH);
-          digitalWrite(LED_VERDE, LOW);
-          digitalWrite(LED_AZUL, LOW);
-        }
-        if(contador = 4){
-          contador = 0;
-        }
-
-
-      }
-
-    }
-
+  if (digitalRead(BOTAO_MODO) == LOW) {
+    manual = !manual;
+    Serial.println(manual);
+    delay(250);
   }
 
+  if (!manual) {
+    Serial.println("Modo Automatico");
+
+    if(ldrConvertido > 200) {
+
+      digitalWrite(LED_AZUL, HIGH);
+      digitalWrite(LED_VERMELHO, LOW);
+      digitalWrite(LED_VERDE, LOW);
+      Serial.println("LED AZUL LIGADO!");
+
+    } 
+    else if (ldrConvertido < 200 && ldrConvertido > 100) {
+
+      digitalWrite(LED_VERMELHO, HIGH);
+      digitalWrite(LED_AZUL, LOW);
+      digitalWrite(LED_VERDE, LOW);
+      Serial.println("LED VERMELHO LIGADO!");
+
+    } 
+    else if (ldrConvertido < 100 && ldrConvertido > 1 ) {
+
+      digitalWrite(LED_VERDE, HIGH);
+      digitalWrite(LED_VERMELHO, LOW);
+      digitalWrite(LED_AZUL, LOW);
+      Serial.println("LED VERDE LIGADO!");
+
+    }
+
+  } else { 
+    Serial.println("Modo Manual!");
+
+    if (digitalRead(BOTAO_CORES) == LOW) {
+      contador++;
+      delay(250);
+    }
+
+    if (contador == 4) {
+      contador = 1;
+    }
+
+    if (contador == 1) {
+      digitalWrite(LED_VERMELHO, HIGH);
+      digitalWrite(LED_AZUL, LOW);
+      digitalWrite(LED_VERDE, LOW);
+      Serial.println("Led Vermelho ligado!");
+    }
+    else if (contador == 2) {
+      digitalWrite(LED_AZUL, HIGH);
+      digitalWrite(LED_VERMELHO, LOW);
+      digitalWrite(LED_VERDE, LOW);
+      Serial.println("Led Azul ligado!");
+    }
+    else if (contador == 3) {
+      digitalWrite(LED_VERDE, HIGH);
+      digitalWrite(LED_AZUL, LOW);
+      digitalWrite(LED_AZUL, LOW);
+      Serial.println("Led Verde ligado!");
+    }
+
+    
+  
+  }
+
+ 
 }
